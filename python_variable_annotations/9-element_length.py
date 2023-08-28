0551f5024341c485ef1090278abbb9624c804fd9
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-28 14:34:08 +0200

    Sequence and iterable used

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function make_multiplier that takes a float """
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
