7180d364d20abf87a8f773027da2b1f90ff4bb63
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-29 14:13:49 +0200

    Correction add import generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,7 +2,7 @@
 """coroutine called async_generator that takes no arguments."""
 import asyncio
 import random
-
+from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:    "An asynchronous generator that yields float values."
     for i in range(0, 10):
