27f211bf41e6fc654de06ced95b61ed4909585e5
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-29 09:41:37 +0200

    Task 0 and readme

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     "The basics of async"
-    # Generate a random delay within the given max delay
+    # Generate a random delay within the given max_delay
     delay = random.uniform(0, max_delay)
     # Asynchronously sleep for the generated delay
     await asyncio.sleep(delay)
---
+++ python_async_function/README.md
@@ -0,0 +1,2 @@
+Update
+projet 2
