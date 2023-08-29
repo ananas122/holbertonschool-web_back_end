e809307e34004846098435d6a38dfc66bb5bf8a2
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-29 09:38:57 +0200

    Task 0: async syntax

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""asynchronous coroutine"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    "The basics of async"
+    # Generate a random delay within the given max delay
+    delay = random.uniform(0, max_delay)
+    # Asynchronously sleep for the generated delay
+    await asyncio.sleep(delay)
+    return delay
