df90a44b5dee4c660a5832617becebfb403d2262
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-29 14:39:24 +0200

    Correction loop

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,9 +6,8 @@ from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
     "Returns list of random float numbers."
-    result = []
-    async for i in async_generator():
-        result.append(i)
+    result = [num async for num in async_generator()]
     return result
