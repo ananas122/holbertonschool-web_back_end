46ce3aec79b69d6817228202ef0d2f7fc9e028ee
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-29 14:12:47 +0200

    Correction add generator float

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,8 +4,7 @@ import asyncio
 import random
 
 
-async def async_generator():
-    "An asynchronous generator that yields float values."
+async def async_generator() -> Generator[float, None, None]:    "An asynchronous generator that yields float values."
     for i in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
