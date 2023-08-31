38a2495a6bc33108e795c642e97873dfdd0b179d
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-31 09:49:05 +0200

    pycodestyle

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -8,8 +8,8 @@ wait_random = __import__('3-tasks').wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    "Waits for 'n' tasks to complete, each task waiting for a random amount of time up to 'max_delay"
-    # Create a list of tasks, each task executing the wait_random(max_delay) function
+    "Waits for 'n' tasks to complete 'max_delay"
+    # Create a list of tasks, each task executing the max_delay
     tasks = [wait_random(max_delay) for _ in range(n)]
     # Use asyncio.gather to wait for all tasks to complete
     delays = await asyncio.gather(*tasks)
