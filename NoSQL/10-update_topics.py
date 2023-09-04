680d41e97c28060dbe12426b36a2ce800a1610e8
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-04 14:18:36 +0200

    Update topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function that changes all topics of a school document based on the name"""
+import pymongo
+
+def update_topics(mongo_collection, name, topics):
+    "change topics update"
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
