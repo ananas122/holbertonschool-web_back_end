a0a9b4690394dfa9ab22e93d1384393d3b0259e5
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-04 14:34:18 +0200

    return list with topics

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-""" 11-main """
-from pymongo
+"""returns the list of school having a specific topic"""
+
 
 def schools_by_topic(mongo_collection, topic):
     "returns the list of school having a specific topic"
-    schools = mongo_collection.find({"topics": topic})
-    return list(schools)
+    return mongo_collection.find({'topics': topic})
