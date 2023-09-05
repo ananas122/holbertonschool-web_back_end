f273da0ee1cab9f7e69b4be6587ceb526c7ebd7e
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-05 10:24:36 +0200

    Pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,5 +6,6 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     "return index range"
     start_index = (page - 1) * page_size
-    end_index= start_index + page_size
+    end_index = start_index + page_size
+
     return (start_index, end_index)
