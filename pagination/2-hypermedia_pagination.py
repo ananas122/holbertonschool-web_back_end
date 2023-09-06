26b7f293f74857ab37ffdc76efcc1a0697169446
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-06 10:42:06 +0200

    Task return changes

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -36,23 +36,22 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         # Call get_page method to retrieve the dataset page
-        dataset_items = len(self.dataset())
+        assert isinstance(page, int) and page > 0, "page must be a positive integer"
+        assert isinstance(page_size, int) and page_size > 0, "page_size must be a positive integer"
+
         data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
 
-        # Calculate the number of pages based on the total rows and page size
-        total_pages = math.ceil((len(self._data) - 1) / page_size)
-
-        # Create the hyper dictionary
-        data  {
+        return  {
             "page_size": len(data),
             "page": page,
             "data": data,
-            "next_page": page + 1 if page < total_pages else None,
-            "prev_page": page - 1 if page > 1 else None,
-            "total_pages": total_pages,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
         }
-
-        return data
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     "return index range"
