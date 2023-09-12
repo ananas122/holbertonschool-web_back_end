edeb5a445ea16f9eeee70fbc8c853a854ece27c7
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-12 09:54:11 +0200

    Creates var in block correction

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,17 @@
+/**
+ * The function `taskBlock` returns an array containing two boolean values, `task` and `task2`, based
+ * on the value of the `trueOrFalse` parameter.
+ * @param trueOrFalse - A boolean value that determines whether the task variables should be set to
+ * true or false.
+ * @returns an array containing the values of the variables `task` and `task2`.
+ */
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
