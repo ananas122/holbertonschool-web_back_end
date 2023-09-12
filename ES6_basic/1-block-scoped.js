320efd97b7613b2352f9a975951e0033cccab577
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-12 12:43:09 +0200

    Correction with eslint

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,9 +1,3 @@
-/**
- * The function `taskBlock` returns an array containing two boolean values, `task` and `task2`, based
- * on the input `trueOrFalse`.
- * @param trueOrFalse - A boolean value that determines whether to execute the if statement or not.
- * @returns an array containing the values of `task` and `task2`.
- */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
@@ -11,6 +5,7 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
+    return [task, task2];
   }
 
   return [task, task2];
