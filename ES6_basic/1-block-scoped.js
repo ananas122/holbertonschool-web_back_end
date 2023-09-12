cca7cff99ad6b6c86b7aeb8867172cc8deec5513
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-12 10:39:24 +0200

    Correction

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,3 +1,9 @@
+/**
+ * The function `taskBlock` returns an array containing two boolean values, `task` and `task2`, based
+ * on the input `trueOrFalse`.
+ * @param trueOrFalse - A boolean value that determines whether to execute the if statement or not.
+ * @returns an array containing the values of `task` and `task2`.
+ */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
