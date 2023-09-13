f00e72d3f9fce0582e0745bf64a1c2d9fddc6cdb
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-13 19:28:13 +0200

    Correction

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,18 +1,16 @@
-class Building {
+export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage === 'undefined') throw new Error('Class extending Building must override evacuationWarningMessage');
     this._sqft = sqft;
   }
 
+  /* getter sqft */
   get sqft() {
     return this._sqft;
   }
 
-  set sqft(sqft) {
-    this._sqft = sqft;
+  /* setter sqft */
+  set sqft(value) {
+    this._sqft = value;
   }
 }
-
-export default Building;
