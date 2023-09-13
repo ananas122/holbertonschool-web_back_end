513233b8e12ab845317d31f8cc2fdae4606b1f89
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-14 00:10:16 +0200

    Correction delete setter

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,25 +3,12 @@ import Building from './5-building.js';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft); // Call the constructor of the parent class
-    this._floors = floors;
-  }
-
-  // getter and setter sqft
-  get sqft() {
-    return this._sqft;
-  }
-
-  set sqft(value) {
-    this._sqft = value;
+    this._floors = floors; // Assign the value of floors to the _floors attribute
   }
 
   // getter and setter floor
   get floors() {
     return this._floors;
-  }
-
-  set floors(value) {
-    this._floors = value;
   }
 
   // Override the evacuationWarningMessage method
