041bb672701c4f7bb41c8645e206f90b70e74218
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-13 23:28:24 +0200

    Use clone clar()

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+export default class Car {
+    constructor(brand, motor, color) {
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
+    }
+  
+    // Méthode 
+    cloneCar() {
+      // Crée une nouvelle instance de la même classe avec les mêmes valeurs
+      return new this.constructor(this._brand, this._motor, this._color);
+    }
+  }
+  
