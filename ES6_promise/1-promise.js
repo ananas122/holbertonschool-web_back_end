b06f89f6f13fe928b5ae1cf29b05a5e864cc976c
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-02 01:17:55 +0200

    Return promise with conditions succes and false

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+/* Append three handlers to the function */
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
+
