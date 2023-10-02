dce6f275a01f57a4aa500480fe56c155d4e20077
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-02 13:00:19 +0200

    Handles the signup

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+/* Handles the signup process for a user profile by uploading a photo and creating a user */
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup () {
+  return Promise.all([uploadPhoto(), createUser()])
+    // destructuration qui extrait les results des promesses ds le tableau
+    .then(([uploadPhotoResult, createUserResult]) => {
+      console.log(`${uploadPhotoResult.body} ${createUserResult.firstName} ${createUserResult.lastName}`);
+    })
+    .catch((error) => {
+      console.error('Signup system offline', error);
+    });
+}
