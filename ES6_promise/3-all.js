5333097680e5b5fa4791d047b0a72c122a517db2
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-02 15:06:30 +0200

    Correction console error and add comment

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,14 +1,12 @@
 /**
  * Handles the signup process for a user profile by uploading a photo and creating a user.
  * @returns {Promise} A promise that resolves when the signup process is successful.
- * @example
  * handleProfileSignup()
  *   .then(() => console.log('Signup successful'))
  *   .catch(() => console.log('Signup system offline'));
- */
-import { uploadPhoto, createUser } from './utils';
+ */import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup () {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((result) => {
       console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
