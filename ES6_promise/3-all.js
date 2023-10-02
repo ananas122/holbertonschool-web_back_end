3a9f6cbdd4a783de3b67fb80edeaa5e91ef4cf95
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-02 14:51:17 +0200

    Correction msg d erreur

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,19 +1,11 @@
 /* Handles the signup process for a user profile by uploading a photo and creating a user */
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
+export default function handleProfileSignup () {
   return Promise.all([uploadPhoto(), createUser()])
     // destructuration qui extrait les results des promesses ds le tableau
     .then(([uploadPhotoResult, createUserResult]) => {
       console.log(`${uploadPhotoResult.body} ${createUserResult.firstName} ${createUserResult.lastName}`);
     })
-    .catch((error) => {
-      if (error instanceof Error) {
-        console.error('Signup system offline', error.message);
-        throw error; // Renvoie l'erreur pour que la promesse soit rejetée
-      } else {
-        console.error('Signup system offline', error);
-        throw new Error('Une erreur inattendue est survenue.');
-      }
-    });
+    .catch((error) => console.error('Signup system offline', error));
 }
