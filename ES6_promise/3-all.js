27b7c7d6d055323e9bd9f4bd369990455f5f7a5c
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-02 15:02:16 +0200

    Correction msg d erreur

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,17 @@
-/* Handles the signup process for a user profile by uploading a photo and creating a user */
+/**
+ * Handles the signup process for a user profile by uploading a photo and creating a user.
+ * @returns {Promise} A promise that resolves when the signup process is successful.
+ * @example
+ * handleProfileSignup()
+ *   .then(() => console.log('Signup successful'))
+ *   .catch(() => console.log('Signup system offline'));
+ */
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup () {
   return Promise.all([uploadPhoto(), createUser()])
-    // destructuration qui extrait les results des promesses ds le tableau
-    .then(([uploadPhotoResult, createUserResult]) => {
-      console.log(`${uploadPhotoResult.body} ${createUserResult.firstName} ${createUserResult.lastName}`);
+    .then((result) => {
+      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
-    .catch((error) => console.error('Signup system offline', error));
+    .catch(() => console.log('Signup system offline'));
 }
