5deb34984d42e465f868613bce936cd0aa7329b8
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-02 10:54:38 +0200

    add then catch finally

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error('Got a response from the API'));
-    }
-  });
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
