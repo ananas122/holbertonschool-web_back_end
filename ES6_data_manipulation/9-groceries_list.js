ad90e5838abf1d7ef93086d4b71c85e7a764c9ba
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-03 22:49:43 +0200

    Correction eslint

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,3 +1,10 @@
+/**
+* Creates a groceries list as a Map.
+* More detailed explanation:
+* - Creates a new Map representing a groceries list.
+* - The Map contains grocery items as keys and quantities as values.
+* @returns {Map} The groceries list Map.
+*/
 export default function groceriesList() {
   return new Map([
     ['Apples', 10],
