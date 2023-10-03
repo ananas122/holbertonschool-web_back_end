e1269572912f6b8f7239a9e77f501554b2ba2645
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-03 23:53:49 +0200

    Correction eslint

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,12 +1,14 @@
 /**
-* Checks if a set contains all values from an array.
-* Validates array input and returns a boolean indicating if the set contains all array values.
-* @param {Set} set - The set to check
-* @param {Array} array - The array of values to check for
-* @returns {boolean} True if set contains all array values, false otherwise.
+ * Checks if a set contains all array values.
+ *
+ * @param {Set} set
+ * @param {Array} array
+ * @returns {boolean}
 */
+
 export default function hasValuesFromArray(set, array) {
-  if (!Array.isArray(array)) { return false; }
-  // check if set contains each value
+  if (!Array.isArray(array))
+  return false
+  // Verify if  set contains each value
   return array.every(val => set.has(val));
 }
