7f2eed395f28fc757bb65cc681fcd5975702a488
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-03 15:25:19 +0200

    Correction eslint

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,10 @@
-export default function getStudentsByLocation (students, city) {
+/**
+ * Returns an array of students from a specific location.
+ * @param {Array} students - An array of student objects.
+ * @param {string} city - The city to filter the students by.
+ * @returns {Array} - An array of student objects from the specified location.
+ */
+
+export default function getStudentsByLocation(students, city) {
   return students.filter((student) => student.location === city);
 }
