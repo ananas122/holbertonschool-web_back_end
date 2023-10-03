bd38ac954e42b8d78b9dd74d0abac013724c72f5
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-03 23:30:59 +0200

    Clean set

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+/**
+* Filters a set by prefix and joins values with dashes.
+* Takes a set and string prefix, filters set to values starting with prefix,
+* slices prefix from values, and joins with dashes.
+* @param {Set} set The input set to filter and join.
+* @param {string} prefix The prefix to filter values by.
+* @returns {string} The filtered set values joined by dashes.
+*/
+export default function cleanSet(set, prefix) {
+  if (!prefix || !set || typeof set !== 'object') { return ''; }
+
+  return Array.from(set)
+    .filter(value => value.startsWith(prefix))
+    .map(value => value.slice(prefix.length))
+    .join('-');
+}
