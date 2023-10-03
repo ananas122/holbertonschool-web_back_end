0072421ceb7c59791831f4080f111ba89669e31f
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-03 11:19:47 +0200

    Correction return

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,12 @@
-export default function getListStudents() {
+/**
+* Returns a list of student objects.
+* @returns {Array} An array of student objects.
+*/
+export default function getListStudents () {
   const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ];
   return students;
 }
