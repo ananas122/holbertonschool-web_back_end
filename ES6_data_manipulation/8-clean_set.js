75b1a968516777088b3dfee758ff74f6db9c029e
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-04 10:52:04 +0200

    Correction condition

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,27 @@
-export default function cleanset(set, startString) {
-  if (!startString || !set || typeof set !== 'object') { return ''; }
+/**
+* Filters a set by a start string and joins values with dashes.
+* Takes a set and string startString, filters set to values starting with 
+* startString, removes startString from those values, and joins the filtered
+* values into a string separated by dashes.
+* @param {Set} set - The input set to filter
+* @param {string} startString - The string to filter the set by
+* @returns {string} The filtered set values joined by dashes
+*/
+export default function cleanSet(set, startString) {
+ const string = [];
 
-  return Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
-    .join('-');
+ if (
+   typeof set !== 'object'
+   || typeof startString !== 'string'
+   || startString.length === 0
+ ) {
+   return '';
+ }
+
+ for (const item of set) {
+   if (item && item.startsWith(startString)) {
+     string.push(item.slice(startString.length));
+   }
+ }
+ return string.join('-');
 }
