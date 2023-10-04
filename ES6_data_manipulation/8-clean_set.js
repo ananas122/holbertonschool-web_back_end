be480f42f1887721a502ac32967793e7c791c0c3
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-04 10:21:15 +0200

    Correction arg

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,3 @@
-/**
-* Filters a set by prefix and joins values with dashes.
-* Takes a set and string prefix, filters set to values starting with prefix,
-* slices prefix from values, and joins with dashes.
-* @param {Set} set The input set to filter and join.
-* @param {string} startString The prefix to filter values by.
-* @returns {string} The filtered set values joined by dashes.
-*/
 function cleanSet(set, startString) {
   if (!startString) {
     return '';
