8ab1a7423702d1a6f83e66b7bfe52e1cd09f911a
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-04 09:56:33 +0200

    Typed array

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,19 @@
+/**
+ * Creates a DataView with Int8 value at position.
+ * @param {number} length
+ * @param {number} position
+ * @param {number} value
+ * @returns {DataView}
+ * @throws {Error} If position outside range
+ */
+export default function createInt8TypedArray(length, position, value) {
+  try {
+    const buffer = new ArrayBuffer(length);
+    //permet de lire et écrire dans le tampon.
+    const int8 = new DataView(buffer)
+    int8.setInt8(position, value);
+    return int8;
+  } catch (err) {
+    throw Error('Position outside range');
+  }
+}
