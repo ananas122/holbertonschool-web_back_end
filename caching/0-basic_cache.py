40b83115a22aded7303e6cd94ba7999a0bd22fcd
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-05 11:21:56 +0100

    Task 0 - Basic dictionary

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -4,10 +4,11 @@
 
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
     """ 
     BasicCache caching system.
-    
+
     This class represents a basic caching system that inherits from BaseCaching.
     It uses a dictionary to store data without any size limit.
     """
@@ -15,7 +16,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """
         Add or update an item in the cache.
-        
+
         Args:
             key: The key for the item to be added or updated.
             item: The value of the item.
@@ -27,15 +28,14 @@ class BasicCache(BaseCaching):
     def get(self, key):
         """
         Retrieve an item from the cache by its key.
-        
+
         Args:
             key: The key of the item to retrieve.
-        
+
         Returns:
-            The value associated with the key in the cache, or None if the key is absent or None.
+            The value associated with the key in the cache, or None if the key
+            is absent or None.
         """
         # Return the value associated with the key if it exists in the cache
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
-        return None
-
