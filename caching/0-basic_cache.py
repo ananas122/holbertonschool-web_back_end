9841b1dbec50531234028b022630f0d3a03dbad6
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-05 12:14:48 +0100

    Task 0 - Basic dictionary

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,40 +1,29 @@
-#!/usr/bin/python3
-""" BaseCaching module
+#!/usr/bin/env python3
+""" Basic dictionary
 """
-
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
-    """ 
-    BasicCache caching system.
-    
-    This class represents a basic caching system that inherits from BaseCaching.
-    It uses a dictionary to store data without any size limit.
+    """ BaseCaching defines:
+       data are stored (in a dictionary)
     """
 
+    def __init__(self):
+        """ Initiliaze
+        """
+        self.cache_data = {}
+
     def put(self, key, item):
+        """ Add an item in the cache
         """
-        Add or update an item in the cache.
-        
-        Args:
-            key: The key for the item to be added or updated.
-            item: The value of the item.
-        """
-        # Add the item only if neither the key nor the value is None
-        if key is not None and item is not None:
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
+        """ Get an item by key
         """
-        Retrieve an item from the cache by its key.
-        
-        Args:
-            key: The key of the item to retrieve.
-        
-        Returns:
-            The value associated with the key in the cache, or None if the key
-            is absent or None.
-        """
-        # Return the value associated with the key if it exists in the cache
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
