dbd7bb4e98a6d6bd14d0050926a2b050b92595ac
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-19 11:17:20 +0100

    Regex

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator) -> str:
+    """
+    returns the log message obfuscated
+    """
+    for i in fields:
+        message = re.sub(i, redaction, message)
+    return message
