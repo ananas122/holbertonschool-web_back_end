dd17d4631b63a1792ce793b0cbc6dfb321cb3c79
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-19 11:28:44 +0100

    Regex

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python3
-"""
-Main file
-"""
-
 import re
-from typing import List
-
+    """
+    Regex 
+    """
 
 def filter_datum(fields, redaction, message, separator) -> str:
     """
     returns the log message obfuscated
     """
-    for i in fields:
-        message = re.sub(i, redaction, message)
-    return message
+    return re.sub('|'.join(fields), redaction, message)
