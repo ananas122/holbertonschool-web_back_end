6e8a892cf148ba7066f6797ffc29f4f7eae6511d
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-20 15:01:15 +0100

    Adjust authentication requirements for specific routes

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,42 +8,21 @@ from typing import List, TypeVar
 
 class Auth:
     """Class for managing API authentication."""
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Check if authentication is required for a given path.
-
-        Args:
-            path (str): The path for which authentication is being checked.
-            excluded_paths (List[str]): List of paths excluded from auth.
-
-        Returns:
-            bool: True if authentication is required, False otherwise.
-        """
-        # This method will be implemented later
-        return False
+        if path is None:
+            return True
+        if not excluded_paths or excluded_paths is None:
+            return True
+        for excluded_path in excluded_paths:
+            # Check if the given path is=excludedpath or it starts with
+            if path == excluded_path or path.startswith(excluded_path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """Retrieve the Authorization header from the Flask request.
-
-        Args:
-            request: The Flask request.
-
-        Returns:
-        str: The Authorization header from the request if present,
-        None otherwise.
-        """
         if not request:
             return None
         return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Return the current user based on the Flask request.
-
-        Args:
-            request: The Flask request.
-
-        Returns:
-            TypeVar('User'): The current user, None if unavailable.
-        """
-        # This method will be implemented later
         return None
