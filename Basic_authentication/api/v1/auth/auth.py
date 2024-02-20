2c6f0422da3f01f2c8d3c14021e4ffd0a8bb9757
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-20 12:17:41 +0100

    Auth class

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,19 +1,44 @@
-#!/usr/bin/env python3
-"""
-Auth class
-"""
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
+    """Class for managing API authentication."""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if authentication is required for a given path.
+
+        Args:
+            path (str): The path for which authentication is being checked.
+            excluded_paths (List[str]): List of paths excluded from authentication.
+
+        Returns:
+            bool: True if authentication is required, False otherwise.
+        """
+        # This method will be implemented later
         return False
 
     def authorization_header(self, request=None) -> str:
+        """Retrieve the Authorization header from the Flask request.
+
+        Args:
+            request: The Flask request.
+
+        Returns:
+            str: The Authorization header from the request if present, None otherwise.
+        """
         if not request:
             return None
         return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """Return the current user based on the Flask request.
+
+        Args:
+            request: The Flask request.
+
+        Returns:
+            TypeVar('User'): The current user, None if unavailable.
+        """
+        # This method will be implemented later
         return None
