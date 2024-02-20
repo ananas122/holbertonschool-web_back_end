37859c950ac65525fa26c72455e1652b91078696
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-20 15:04:00 +0100

    Adjust authentication requirements for specific routes

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,17 +8,22 @@ from typing import List, TypeVar
 
 class Auth:
     """Class for managing API authentication."""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        # Check if the path is None, in which case authentication is required
-        if path is None:
+        """Check if authentication is required for a given path."""
+        # Check if the path is None or excluded_paths is None or empty
+        if path is None or not excluded_paths:
             return True
-        if not excluded_paths or excluded_paths is None:
-            return True
+
+        # Iterate through the excluded paths
         for excluded_path in excluded_paths:
-            # Check if the given path is=excludedpath or it starts with
+            # Check if the path is equal to an excluded path or if it starts with an excluded path
             if path == excluded_path or path.startswith(excluded_path):
                 return False
+
+        # Authentication is required for the given path
         return True
+
 
     def authorization_header(self, request=None) -> str:
         if not request:
