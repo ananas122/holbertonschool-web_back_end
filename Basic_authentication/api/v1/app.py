e0f52ec259ec98183c7c82bcd6e753ce56040621
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-20 23:44:42 +0100

    Correction variable auth

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -12,6 +12,14 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
+auth = os.getenv('AUTH_TYPE')
+if auth == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif auth:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
 
 
 @app.before_request
@@ -27,11 +35,15 @@ def before_request():
         '/api/v1/forbidden/'
     ]
 
-    if request.path not in excluded_paths:
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
 
 
 @app.errorhandler(404)
