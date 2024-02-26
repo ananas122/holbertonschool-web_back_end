c6170f1b220b33d3c3c54da7ed0ce9dfdf37b686
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-26 15:29:13 +0100

    find user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -46,7 +46,7 @@ class DB:
     def find_user_by(self, **kwargs) -> User:
         """Find a user in the database by specified criteria."""
         try:
-            # Effectue une requ ds db pr récupérer le 1er user = critères kwargs
+            # Effectue 1requ ds db pr récup 1er user = critères kwargs
             user = self._session.query(User).filter_by(**kwargs).first()
             if user is None:
                 raise NoResultFound(
