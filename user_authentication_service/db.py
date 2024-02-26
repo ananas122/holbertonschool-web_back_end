475189f34a8f8d150824e74f6425364c79d7a740
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-26 12:46:46 +0100

    create user

---
+++ user_authentication_service/db.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import Base, User  # Assurez-vous d'importer User ici
+
+
+class DB:
+    """DB class"""
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance"""
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object"""
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds a new user to the database and returns it."""
+        # Create a new User instance with the provided email and hashed password
+        user = User(email=email, hashed_password=hashed_password)
+        # Add the User instance to the current SQLAlchemy session
+        self._session.add(user)
+        # Commit the changes to the database to persist the new user
+        self._session.commit()
+        # Return the User instance, now with an ID assigned by the database
+        return user
