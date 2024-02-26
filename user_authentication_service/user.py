27f62a4b1c8183aa751736d91323ea758d91a51e
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-26 12:18:28 +0100

    User model sqlalchemy

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python3
-"""0. User model
-"""
+"""Class User for ORM"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
 
 
 class User(Base):
-    """User class
-
-    Args:
-        Base (declarative_base): Base class
-
-    Returns:
-        User: User class
-    """
+    """Representation of a user """
     __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(length=250), unique=True, index=True, nullable=False)
-    hashed_password = Column(String(length=250), nullable=False)
-    session_id = Column(String(length=250), nullable=True)
-    reset_token = Column(String(length=250), nullable=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
