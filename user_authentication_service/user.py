6400db7beef90c5c79af75952d43509ef0d3adb4
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-26 12:11:40 +0100

    User model sqlalchemy

---
+++ user_authentication_service/user.py
@@ -0,0 +1,19 @@
+# user.py
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        return "<User(id='%s', email='%s', hashed_password='%s')>" % (
+            self.id, self.email, self.hashed_password)
