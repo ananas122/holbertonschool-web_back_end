7b7e00c27f0ed8b464441d62493ffce379f9d9af
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-26 17:55:16 +0100

    Update

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python3
 """
-Main file
+encrypting passwords
 """
 import bcrypt
+from db import DB
+from user import User, Base
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
-    """Generate a secure hash of the given password."""
-    # Convertir le mot de passe en tableau de bytes
-    password_bytes = password.encode('utf-8')
-    # Générer le sel
-    salt = bcrypt.gensalt()
-    # Hacher le mot de passe
-    hashed_password = bcrypt.hashpw(password_bytes, salt)
-    return hashed_password
+    """Encrypting passwords"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Generate a UUID"""
+    return str(uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user with the database.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+
+        except NoResultFound:
+            passwd: str = _hash_password(password)
+            user = self._db.add_user(email, passwd)
