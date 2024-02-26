500fbe396ee685ea6d480ce9381d11c1aa723a01
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-26 17:33:23 +0100

    Hash password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,15 +4,13 @@ Main file
 """
 import bcrypt
 
+
 def _hash_password(password: str) -> bytes:
     """Generate a secure hash of the given password."""
     # Convertir le mot de passe en tableau de bytes
     password_bytes = password.encode('utf-8')
-
     # Générer le sel
     salt = bcrypt.gensalt()
-
     # Hacher le mot de passe
     hashed_password = bcrypt.hashpw(password_bytes, salt)
-
     return hashed_password
