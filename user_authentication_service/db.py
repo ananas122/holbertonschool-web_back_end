01a12e3180dfab84d53082e9297b96e1852463a8
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-26 12:57:05 +0100

    Create user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,35 +1,46 @@
+#!/usr/bin/env python3
+"""DB module
+"""
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import Base, User  # Assurez-vous d'importer User ici
+from sqlalchemy.orm.exc import NoResultFound
+
+from user import Base, User
 
 
 class DB:
-    """DB class"""
+    """DB class
+    """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance"""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object"""
+        """Memoized session object
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a new user to the database and returns it."""
-        # Create a new User instance with the provided email and hashed password
+        """Add a new user to the database.
+
+        Args:
+            email (str): The email of the user.
+            hashed_password (str): The hashed password of the user.
+
+        Returns:
+            User: The User object representing the added user.
+        """
         user = User(email=email, hashed_password=hashed_password)
-        # Add the User instance to the current SQLAlchemy session
         self._session.add(user)
-        # Commit the changes to the database to persist the new user
         self._session.commit()
-        # Return the User instance, now with an ID assigned by the database
         return user
