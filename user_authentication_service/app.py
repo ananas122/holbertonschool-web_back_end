f86236402d49b19cb801573a67e6f593ed5b26c3
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-26 23:14:46 +0100

    pycodestyle

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-"""
-Route module for the API
-"""
-from flask import Flask, jsonify, request
+""" Flask module """
+from flask import Flask, jsonify, request, abort, redirect, make_response
+from sqlalchemy.orm.exc import NoResultFound
 from auth import Auth
 
 app = Flask(__name__)
@@ -64,6 +63,59 @@ def logout() -> str:
         return redirect('GET /')
     else:
         abort(403)
+
+
+@ app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """ Get profile with session id
+    """
+    session_id = request.cookies.get('session_id', None)
+
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    return jsonify({"email": user.email}), 200
+
+
+@app.route('/reset_password', methods=['POST'])
+def reset_password() -> str:
+    """ Get profile with session id"""
+    try:
+        email = request.form['email']
+    except KeyError:
+        abort(403)
+
+    token: str = ''
+    try:
+        token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "reset_token": token}), 200
+
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    """ Change the password"""
+    try:
+        email = request.form['email']
+        reset_tok = request.form["reset_token"]
+        new_pwd = request.form['new_password']
+    except KeyError:
+        abort(403)
+
+    try:
+        AUTH.update_password(reset_tok, new_pwd)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
 
 
 if __name__ == "__main__":
