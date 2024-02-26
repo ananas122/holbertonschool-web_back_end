9a7210f891574a87bad64bcf778f811a8013709f
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-26 14:38:42 +0100

    Correction

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
-""" Database for ORM """
+"""
+DB module
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
 from user import Base, User
 
 
 class DB:
-    """DB class"""
+    """DB class
+    """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance"""
+        """Initialize a new DB instance
+        """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -18,19 +24,18 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object"""
+        """Memoized session object
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a new user to the database and returns it."""
-        # Create a new User instance with the provided email and hashed password
-        new_user = User(email=email, hashed_password=hashed_password)
-        # Add the User instance to the current SQLAlchemy session
-        self._session.add(new_user)
-        # Commit the changes to the database to persist the new user
+        """
+            Add a new user to the database.
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        # Return the User instance, now with an ID assigned by the database
-        return new_user
+        return user
