dc8945b10e27518d05971e0785b81e3b7adefb02
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-02-27 13:19:36 +0100

    Correction requieremnts fails

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -117,6 +117,5 @@ def update_password() -> str:
     return jsonify({"email": email, "message": "Password updated"}), 200
 
 
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
