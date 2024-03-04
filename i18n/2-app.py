eb5ab053c0617aac7060c125b1471e0db201ab74
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-03-04 19:04:48 +0100

    Task 2 get local

---
+++ i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""task 2"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class to setup Babel for English and French"""
+    LANGUAGES = ["en", "fr"]
+    Babel.default_locale = "en"
+    Babel.default_timezone = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """Template for 2-index"""
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Get user locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    host = "0.0.0.0"
+    port = "5000"
+    app.run(host=host, port=port, debug=True)
