58c634af6f4d144fe5672e6110c615729e5405ab
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-03-05 21:05:15 +0100

    Task 6

---
+++ i18n/6-app.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+"""
+Paramétrer les modèles
+"""
+import flask
+from flask import Flask, render_template, g, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+# Utilisateurs fictifs
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+# Configuration de Babel
+
+
+class Config(object):
+    """
+    Configuration de l'application
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route("/", methods=['GET'])
+def index():
+    """template """
+    return render_template('5-index.html')
+
+
+def get_user() -> dict:
+    """Retrieve user based on login_as parameter"""
+    # Get the value of the login_as prm from the request's query prm
+    user_id = request.args.get('login_as')
+    try:
+        # Try to convert the user_id to an integer
+        user_id = int(user_id)
+        # Check if the user_id exists in the users dictionary
+        if user_id in users:
+            # Return the user dictionary corresponding to the user_id
+            return users[user_id]
+    # if there's an error during conversion or if user_id is not found in users
+    except (ValueError, TypeError):
+        # If there's an error, do nothing and proceed to the next step
+        pass
+    # If user_id is not found or there's an error
+    return None
+
+
+@app.before_request
+def before_request():
+    """ Définir l'utilisateur global s'il est connecté"""
+    user = get_user()
+    if user:
+        g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine the user's preferred locale
+    """
+    # Check if the locale is specified in the URL parameters
+    user_locale = request.args.get('locale')
+    if user_locale in app.config['LANGUAGES']:
+        return locale
+
+    # Check if the user is logged in and has a preferred locale
+    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
+        return g.user['locale']
+
+    # Check the request header for the preferred locale
+    request_locale = request.headers.get('Accept-Language')
+    if request_locale:
+        request_locale = request_locale.split(',')[0].split(';')[0]
+        if request_locale in app.config['LANGUAGES']:
+            return request_locale
+
+    # Return the default locale if none of the above conditions are met
+    return app.config['BABEL_DEFAULT_LOCALE']
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
