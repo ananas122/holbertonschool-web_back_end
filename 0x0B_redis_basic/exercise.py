f17bddd50ba0e348916d9220eebcc7ccc038f1bc
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-03-20 17:46:21 +0100

    task 0

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,17 @@
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        # Generate random key
+        key = str(uuid.uuid4())  
+        # Store data in Redis with random key
+        self._redis.set(key, data)  
+        # Return the generated key
+        return key  
