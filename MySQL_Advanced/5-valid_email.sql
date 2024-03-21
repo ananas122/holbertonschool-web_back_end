0a556bb6661d32ee91be71122205eb4e95377456
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-03-21 18:00:48 +0100

    task 5

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -5,9 +5,10 @@ CREATE TRIGGER reset_valid_email_on_email_change
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
+IF NEW.email <> OLD.email THEN
+THEN        
         SET NEW.valid_email = 0;
-    END IF;
+        END IF;
 END;
 //
 DELIMITER ;
