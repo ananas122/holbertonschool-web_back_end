13900b559a98515c73e3206f119f2b18c8fe5234
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-03-21 16:08:17 +0100

    Task 3

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
 -- Search bands with style Glam rock
 -- Durantion current band
-SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+WHERE style LIKE '%Glam rock%';
