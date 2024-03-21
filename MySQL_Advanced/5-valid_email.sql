0176f1a51ffe2dabc5f75b413aeffa9662524064
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-03-21 18:20:34 +0100

    task 5

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,14 +1,13 @@
 -- path: 5-store.sql
 -- Description: This file resets the attribute valid_email only when the email has been changed.
 DELIMITER //
-CREATE TRIGGER reset_valid_email_on_email_change
+CREATE TRIGGER reset_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-IF NEW.email <> OLD.email THEN
-THEN        
+    IF OLD.email != NEW.email THEN
         SET NEW.valid_email = 0;
-        END IF;
+    END IF;
 END;
 //
 DELIMITER ;
