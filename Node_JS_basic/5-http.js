cfede3587fcfeeb41463e78f0167b587bc85bd47
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-03-22 22:21:50 +0100

    Update

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,22 +1,25 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
+const hostname = '127.0.0.1';
 const port = 1245;
-const app = http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+const DB = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
-    res.write('Hello Holberton School!');
-  }
-  if (req.url === '/students') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
-    try {
-      const data = await countStudents(process.argv[2]);
-      res.end(`${data.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
-    }
+    countStudents(DB).then((result) => {
+      res.end(result.join('\n'));
+    }).catch((error) => {
+      res.end(`${error.message}`);
+    });
   }
-  res.end();
-}).listen(port);
+});
+
+app.listen(port, hostname);
 
 module.exports = app;
