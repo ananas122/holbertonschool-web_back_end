c76d16df025f57fc113caee0801684417129e971
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-03-22 22:29:43 +0100

    Update

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,19 +1,34 @@
-import express from 'express';
-import AppController from '../controllers/AppController';
-import StudentController from '../controllers/StudentsController';
+import readDatabase from '../utils';
 
-const router = express.Router();
+export default class StudentsController {
+  static getAllStudents(request, response, DB) {
+    readDatabase(DB).then((result) => {
+      const students = [];
+      students.push('This is the list of our students');
+      Object.keys(result).sort().forEach((key) => {
+        students.push(`Number of students in ${key}: ${result[key].length}. List: ${result[key].join(', ')}`);
+      });
+      response.status(200);
+      response.send(students.join('\n'));
+    }).catch((error) => {
+      response.status(500);
+      response.send(error.message);
+    });
+  }
 
-router.get('/', (req, res) => {
-  AppController.getHomepage(req, res);
-});
-
-router.get('/students', (req, res) => {
-  StudentController.getAllStudents(req, res, process.argv[2]);
-});
-
-router.get('/students/:major', (req, res) => {
-  StudentController.getAllStudentsByMajor(req, res, process.argv[2]);
-});
-
-export default router;
+  static getAllStudentsByMajor(request, response, DB) {
+    const { major } = request.params;
+    if (major !== 'CS' && major !== 'SWE') {
+      response.status(500);
+      response.send('Major parameter must be CS or SWE');
+    } else {
+      readDatabase(DB).then((result) => {
+        response.status(200);
+        response.send(`List: ${result[major].join(', ')}`);
+      }).catch((error) => {
+        response.status(500);
+        response.send(error.message);
+      });
+    }
+  }
+}
