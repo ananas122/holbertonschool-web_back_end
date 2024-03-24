e9fd268e7f9437527700fe8749498c9314591b09
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-03-25 00:37:34 +0100

    add const mocha

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,4 +1,7 @@
 function calculateNumber(a, b) {
+    const aNum = Number(a);
+    const bNum = Number(b);
+    
     if (Number.isNaN(aNum) || Number.isNaN(bNum))
         throw TypeError('Parameters must be numbers');
 
