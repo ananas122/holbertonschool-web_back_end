168aedc9c36ef537df7092eaf08104d837775d9d
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-03-25 00:32:32 +0100

    add const mocha

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -2,13 +2,29 @@
 
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
-const mocha = require("mocha");
 
 describe('calculateNumber', function () {
-    it('should return the sum of two rounded numbers', function () {
+    it('should return the correct sum for integer numbers', function () {
         assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+
+    it('should round and return the correct sum for non-integer numbers', function () {
         assert.strictEqual(calculateNumber(1, 3.7), 5);
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+
+    it('should throw TypeError if parameters are not numbers', function () {
+        assert.throws(() => {
+            calculateNumber('a', 3);
+        }, TypeError);
+
+        assert.throws(() => {
+            calculateNumber(1, 'b');
+        }, TypeError);
+
+        assert.throws(() => {
+            calculateNumber('1', '2');
+        }, TypeError);
     });
 });
