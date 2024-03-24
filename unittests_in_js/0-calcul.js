4cfd40c4d1d5fdf0ccb0b4f3a2d1e7727461628e
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-03-25 00:30:15 +0100

    add const mocha

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,10 +1,8 @@
 function calculateNumber(a, b) {
-    // Check if a and b are not numbers
-    if (!isNumber(a) || !isNumber(b)) {
+    if (typeof a !== 'number' || typeof b !== 'number' || isNaN(a) || isNaN(b)) {
         throw new TypeError('Parameters must be numbers');
     }
-
-    // Round the numbers and return their sum
     return Math.round(a) + Math.round(b);
 }
+
 module.exports = calculateNumber;
